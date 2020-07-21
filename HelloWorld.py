b8f750f012ae41da1b009d84dbfb6082b8a163b7
cxf213 <cxf213@outlook.com>
2020-07-21 21:42:37 +0800

    7/21练习

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,13 @@
 print("HelloWorld")
-i=10
+def isPalindrome(x: int) -> bool: #回文数判断
+        original_num=x
+        reversed_num = 0
+        while x > 0:
+            reversed_num = reversed_num * 10 + x % 10
+            x //= 10
+        if (reversed_num==original_num):
+            return True
+        else:
+            return False
+
+print(isPalindrome(121))
