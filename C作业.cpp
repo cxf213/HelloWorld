071d72314efe9b28cd4ac83c71a803f863a6e01a
Xiaofeng Chen <cxf213@outlook.com>
2020-10-30 16:49:28 +0800

    Create C作业.cpp

---
+++ C作业.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+	char * str=new char[5];
+	std::cin>>str;
+	
+	for(int i=0;i<5;i++){
+		if(islower(str[i])){
+			str[i]=(str[i]-'a'+4)%26+'a';
+		}
+		else{
+			str[i]=(str[i]-'A'+4)%26+'A';
+		}
+	}
+	
+	printf("%s\n",str);
+	for(char a:str){
+		putchar(a);
+	}
+}
